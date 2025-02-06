138a9411483d2a6b1f8e33acc94d26bcbdc8b2c3
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-02-06 19:30:43 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get('/profile', (req, res) => {
-    const {username} = req.query;
-    console.log(username);
-    console.log("Hello from the profile page" + " " + username);
+// app.get('/profile', (req, res) => {
+//     const {username,age} = req.query;
+//     console.log(username);
+//     res.send("Hello from the profile page" + " " + username + " " + age);
+// })
+
+app.get("/profile/:id/:username", (req, res) =>{
+    const {id,username} = req.params;
+    res.send(Profile page with id: ${id} & username: ${username});
 })
 
-app.listen(3458, (req,res)=>{
+app.listen(3477, (req, res)=>{
     console.log("server started");
 });
